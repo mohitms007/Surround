8bc9f1bed6d12bb025164e405d5322c0429d266a
DragonMaster <ms235389@gmail.com>
2021-02-04 19:21:18 +0530

    added auth

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import useFormValidation from './useFormValidation'
 import validateLogin from "./validateLogin";
+import firebase from '../../firebase'
 
 const INITIAL_STATE = {
     name: "",
@@ -17,11 +18,25 @@ function Login(props) {
         values,
         errors,
         isSubmitting
-    } = useFormValidation(INITIAL_STATE, validateLogin)
+    } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser)
 
     const [login,
         setLogin] = useState(true)
-    console.log(errors)
+    const [firebaseError, setFirebaseError] = useState(null)
+    async function authenticateUser(){
+        const {name, email, password} = values
+        try{
+            const response = login ? 
+            await  firebase.login(email,password)
+             : await firebase.register(name,email,password)
+            props.history.push("/")
+        }catch(e){
+            console.error('Authentication Error',e)
+            setFirebaseError(e.message)
+        }
+       
+    }
+
     return (
         <div className="login-form">
             <h2 className="mv3 tc items-center">{login
@@ -44,6 +59,7 @@ function Login(props) {
                     value={values.email}
                     placeholder="Your Email"
                     autoComplete="off"/> {errors.email && <p className="error-text">{errors.email}</p>}
+                   
                 <input
                     onChange={handleChange}
                     onBlur={handleBlur}
@@ -52,6 +68,7 @@ function Login(props) {
                     name="password"
                     type="password"
                     placeholder="Your Password"/> {errors.password && <p className="error-text">{errors.password}</p>}
+                      {firebaseError  && <p className="error-text">{firebaseError}</p>}
                 <div className="flex inline mt3 btn-container">
                     <button
                         type="submit"
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function useFormValidation(initialState, validate) {
+function useFormValidation(initialState, validate,authenticateUser) {
   const [values, setValues] = React.useState(initialState);
   const [errors, setErrors] = React.useState({});
   const [isSubmitting, setSubmitting] = React.useState(false);
@@ -9,7 +9,7 @@ function useFormValidation(initialState, validate) {
     if (isSubmitting) {
       const noErrors = Object.keys(errors).length === 0;
       if (noErrors) {
-        console.log("authenticated", values);
+        authenticateUser()
         setSubmitting(false);
       } else {
         setSubmitting(false);
@@ -35,7 +35,6 @@ function useFormValidation(initialState, validate) {
     const validationErrors = validate(values);
     setErrors(validationErrors);
     setSubmitting(true);
-    console.log({ values });
   }
 
   return {
